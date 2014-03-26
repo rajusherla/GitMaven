4f3470cde66b483f7302d7adda09d8d08bfdcd9d
rajus <raju.sai12@gmail.com>
2014-03-26 09:27:58 -0500

    second

---
+++ GitMavenTest/src/main/java/com/bizilab/test/GitMavenClientTest.java
@@ -0,0 +1,14 @@
+package com.bizilab.test;
+import com.bizilab.test.GitMaven.App;
+/**
+ * Hello world!
+ *
+ */
+public class GitMavenClientTest 
+{
+    public static void main( String[] args )
+    {
+       App app = new App();
+       System.out.println(app.getName("raju"));
+    }
+}
